a37a0fc3e5c9273a198ab22eb73846ad181b0662
julypeleg25 <2003yam@gmail.com>
2026-01-02 13:34:12 +0200

    updated themes made screen look better

--- app/src/main/java/com/androidapp/tictactoe/state/GameState.kt
+++ app/src/main/java/com/androidapp/tictactoe/state/GameState.kt
@@ -6,4 +6,5 @@ data class GameState (
     var currentPlayer: Char = GameConstants.PLAYER_X,
     val grid: Array<CharArray> =
         Array(GameConstants.BOARD_SIZE) { CharArray(GameConstants.BOARD_SIZE) { GameConstants.EMPTY_CELL } },
-)
+    var isFinished: Boolean = false
+ )
